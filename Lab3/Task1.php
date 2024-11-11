728ffeb4a1a917586587d13c01f53a698ab107f2
SadmanShopan <sadmanshopan786@gmail.com>
2024-11-11 16:33:45 +0600

    Add files via upload

---
+++ Lab3/Task1.php
@@ -0,0 +1,13 @@
+<?php 
+$length =10;
+$width =15;
+
+echo "The area of the rectangle = " . $length * $width;
+echo "<br>";
+echo "The perimetert of the rectangle = " . 2 * ($length + $width);
+
+
+
+
+
+?>
---
+++ Lab3/Task2.php
@@ -0,0 +1,13 @@
+<?php 
+$capital=10000;
+$per =15;
+
+
+echo "The amount of vat is " . $capital*($per/100);
+
+
+
+
+
+
+?>
