e4c2186fb99ebcb607be9a210efa3b57dc088e41
SadmanShopan <sadmanshopan786@gmail.com>
2024-11-24 02:06:14 +0600

    Update name.php

--- Lab3.2/name.php
+++ Lab3.2/name.php
@@ -6,23 +6,47 @@
         $name  =  $_REQUEST['name'];
         
 
+        //A. Cannot be empty
+
         if($name == null){
             echo "Null data found!";
+            //header('location: name.html');
+            
         }
-        else if ($username == $password){
-            //echo "Valid User!";
-            $_SESSION['flag'] = true;
-            header('location: home.php');
-        }else
-        {
+
+        //B. Contains at least two words
+
+        else if(str_word_count($name)<2){
+            echo "Invelid condition.Less than 2  words.";
+            //header('location: name.html');
+        }
+
+        //C. Must start with a letter
+
+        else if (!preg_match('/^[a-zA-Z]/', $name)) {
+            echo "Input must start with a letter.";
+        }
+
+        //D. Can contain a-z, A-Z, period, dash only
+        else if (!preg_match('/^[a-zA-Z.\-\s]+$/', $name)) {
+            echo "Input can only contain letters, periods, dashes, and spaces.";
+        }
+        
+        else if($name != null){
+            echo "Data Submitted succesfully!";
+            //header('location: name.html');
+        }
+        
+        else{
             echo "Invalid user";
         }
-    }else{
+    }
+    
+    else{
         //echo "Invalid request!";
         header('location: name.html');
     }
 
 
-    //print_r($_GET);
-    //echo "Test";
+   
 ?>
